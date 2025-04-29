cf1590816c3aef0f2a154bdd103bd2b9537ca7e8
guille1006 <guilleia1006@gmail.com>
2025-04-29 20:48:47 +0200

    Pyurifaction curro

--- Pyurification.py
+++ Pyurification.py
@@ -1,20 +1,59 @@
 class Pyurification:
-
-    def __init__(self, df, variable_dependiente):
+    def __init__(self, df, v_depend=False, col_depend=False, col_cat=[]):
         self.df = df
-        self.uniques = df.nunique()
         self.len = len(df)
+        self.uniques = {}
+        self.cols = df.columns
+
+    if v_depend:
+        self.v_depend = v_depend
+        self.col_depend = None
+        self.v_independ = df
+        self.col_depend = df.columns
+
+    elif col_depend:
+        self.col_depend = col_depend
+        self.v_depend = df[col_depend]
+        self.col_independ = [x for x in df.columns if not col_depend]    #Posible error
+        self.v_independ = df[self.col_independ]
+
+    else:
+        self.col_depend = None
+        self.v_depend = None
+        self.col_independ = df.columns
+        self.v_independ = df
+
+    self.col_dicotom = []
+    self.col_cat = []
+    self.col_num = []
+#---------------------------------------------------------------------------------
+    def select_type_variables(self, col_cat):
+        col_to_analyze = [x for x in self.col_independ if not in col_cat]
+       
+        for col in col_to_analyze:
+            serie = self.v_independ[col]
+            self.uniques[col] = serie.nunique()
+
+            if self.uniques[col] == 2:
+                self.col_dicotom.append(col)
+                self.df[col] = dicotom_to_number(df[col])
+
+            elif self.uniques[col]>20:
+               
 
 
-        if type(variable_dependiente)==str:
-            self.col_independientes = self.variables_independientes()
-            self.col_dependiente = variable_dependiente
-        
-        else:
-            self.col_dependiente = self.variables_dependiente()
 
-        self.col_numericas = self.variables_numericas()
-        self.col_categoricas = self.variables_categoricas()
-        self.col_dicotomicas = self.variables_dicotomicas()
-        
+#---------------------------------------------------------------------------------
+    def dicotom_to_number(self, series):
+        val_unicos = series.unique()   #Posible error
+        return series.replace({val_unicos[0]:0, val_unicos[1]:1})
 
+
+#---------------------------------------------------------------------------------
+    def show_frecuencys(self, n_groups_max=250):
+        resultados = {}
+        frecuencias = []
+       
+        columns_showed = [x for x in self.columns if x<n_groups_max]
+
+        for cols in columns_showed:
