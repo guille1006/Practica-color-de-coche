59a036d5d1a269e2bfdddc9f027a528d6667af36
guille1006 <guilleia1006@gmail.com>
2025-04-30 00:26:46 +0200

    Pyurifaction4

--- Pyurification.py
+++ Pyurification.py
@@ -1,10 +1,13 @@
 class Pyurification:
-    def __init__(self, df, v_depend=False, col_depend=False, col_cat=[]):
+    def __init__(self, df, v_depend=False, col_depend=False, col_cat=[], umbral_cat=0.05):
         self.df = df
         self.len = len(df)
         self.uniques = {}
         self.cols = df.columns
+        self.umbral_cat = 
 
+
+# Guardado de las variables dependientes e independientes -------------------------------------
         if v_depend:
             self.v_depend = v_depend
             self.col_depend = None
@@ -14,7 +17,7 @@ class Pyurification:
         elif col_depend:
             self.col_depend = col_depend
             self.v_depend = df[col_depend]
-            self.col_independ = [x for x in df.columns if not col_depend]    #Posible error
+            self.col_independ = [x for x in df.columns if x not in col_depend]    #Posible error
             self.v_independ = df[self.col_independ]
 
         else:
@@ -23,12 +26,15 @@ class Pyurification:
             self.col_independ = df.columns
             self.v_independ = df
 
+# Guardado de cada tipo de variable mediante el metodo select_type_variables()
         self.col_dicotom = []
         self.col_cat = []
         self.col_num = []
+        self.select_type_variables(col_cat)
 #---------------------------------------------------------------------------------
     def select_type_variables(self, col_cat):
-        col_to_analyze = [x for x in self.col_independ if not in col_cat]
+        # Ademas de guardar cada columna en su tipo de variable, guarda clos valores unicos en self.uniques
+        col_to_analyze = self.cols
        
         for col in col_to_analyze:
             serie = self.v_independ[col]
@@ -36,10 +42,11 @@ class Pyurification:
 
             if self.uniques[col] == 2:
                 self.col_dicotom.append(col)
-                self.df[col] = dicotom_to_number(df[col])
+                self.df[col] = self.dicotom_to_number(self.df[col])
 
-            elif self.uniques[col]>20:
-                return
+            elif self.uniques[col] > self.umbral_cat*self.len:
+
+                
                
 
 
@@ -56,6 +63,6 @@ class Pyurification:
         resultados = {}
         frecuencias = []
        
-        columns_showed = [x for x in self.columns if x<n_groups_max]
+        columns_showed = [x for x in self.uniques if x<n_groups_max]
 
         for cols in columns_showed:
